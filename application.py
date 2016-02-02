993eca99fffd0042c33b0087455fffad62c19014
Joel Shuman <joel.shumansg@gmail.com>
2016-02-02 08:13:31 -0800

    Idealized settings for app

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ def main():
 @app.route("/recommend", methods=['POST'])
 def recommend():
     user_input = str(request.form['seed'])
-    sent = mc.run(user_input, 2, 3)
+    sent = mc.run(user_input, 1, 2)
 
     return render_template('index.html', recommend=True, sent=sent)
 
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     with open('data/neighbours.pkl') as f:
         neighbor_dict = pickle.load(f)
 
-    md = MarkovDict('data/obama_corpus.txt', 4, gtype='syntax_pos')
+    md = MarkovDict('data/obama_corpus.txt', 3, gtype='syntax_pos')
     mc = MarkovChain(md.api, neighbor_dict)
 
     app.run(host='0.0.0.0', port=8080, debug=True)
